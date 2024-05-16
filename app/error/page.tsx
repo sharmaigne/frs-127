30054a6927e2f3079231e122bd9c412f74677bec
Sharmaigne Angelie Mabano <sharmaignemabano@gmail.com>
2024-05-17 00:12:02 +0800

    feat: confirm email and error routes

--- app/error/page.tsx
+++ app/error/page.tsx
@@ -1,13 +1,20 @@
 import Link from "next/link";
+import maroonBg from "@/public/images/maroon-bg.png";
+
 const ErrorPage = () => {
   return (
-    <div className="flex-col justify-center items-center w-screen h-screen">
-      <h2>Sorry, something went wrong.</h2>
-      <Link href="/">
-        Go back to home.
-      </Link>
+    <div className="flex justify-center items-center h-screen bg-primary-900">
+      <div
+        className="flex-col m-6 p-20 justify-center text-center rounded-2xl"
+        style={{ backgroundImage: `url(${maroonBg.src})` }}
+      >
+        <h2 className="text-light">Sorry, something went wrong.</h2>
+        <Link href="/" className="underline text-accent">
+          Go back to home.
+        </Link>
+      </div>
     </div>
   );
-}
+};
 
 export default ErrorPage;

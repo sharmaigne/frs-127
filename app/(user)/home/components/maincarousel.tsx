6db488cb5c134dc4b52590317571636ac48a93ec
Sharmaigne Angelie Mabano <sharmaignemabano@gmail.com>
2024-05-19 11:17:15 +0800

    feat: working carousel loop

--- app/(user)/home/components/maincarousel.tsx
+++ app/(user)/home/components/maincarousel.tsx
@@ -1,36 +1,48 @@
-import React from 'react';
-import { CarouselItem, CarouselContent, CarouselPrevious, CarouselNext, Carousel } from "@/components/ui/carousel";
+"use client"
+import React from "react";
+import {
+  CarouselItem,
+  CarouselContent,
+  CarouselPrevious,
+  CarouselNext,
+  Carousel,
+} from "@/components/ui/carousel";
+import Autoplay from "embla-carousel-autoplay";
 
 const images = [
-  { src: "./icons/atrium.jpg", alt: "University Campus" },
-  { src: "", alt: "University Campus" },
-  { src: "/placeholder.svg", alt: "University Campus" },
-  { src: "/vercel.svg", alt: "University Campus" },
-  { src: "/atrium.jpg", alt: "University Campus" }
+  { src: "./images/atrium.jpg", alt: "University Campus" },
+  { src: "./images/maroon-bg.png", alt: "University Campus" },
+  { src: "/images/UP-logo.png", alt: "University Campus" },
 ];
 
-const MainCarousel: React.FC = () => (
-  <Carousel className="rounded-xl overflow-hidden">
-    <CarouselContent>
-      {images.map((image, index) => (
-        <CarouselItem key={index}>
-          <img
-            alt={image.alt}
-            className="w-full h-full object-cover"
-            height="500"
-            src={image.src}
-            style={{
-              aspectRatio: "800/500",
-              objectFit: "cover",
-            }}
-            width="800"
-          />
-        </CarouselItem>
-      ))}
-    </CarouselContent>
-    <CarouselPrevious />
-    <CarouselNext />
-  </Carousel>
-);
+const MainCarousel: React.FC = () => {
+  return (
+    <Carousel
+      className="rounded-xl overflow-hidden"
+      plugins={[Autoplay({ delay: 2000})]}
+      opts={{ loop: true }}
+    >
+      <CarouselContent>
+        {images.map((image, index) => (
+          <CarouselItem key={index}>
+            <img
+              alt={image.alt}
+              className="w-full h-full object-cover"
+              height="500"
+              src={image.src}
+              style={{
+                aspectRatio: "800/500",
+                objectFit: "cover",
+              }}
+              width="800"
+            />
+          </CarouselItem>
+        ))}
+      </CarouselContent>
+      <CarouselPrevious />
+      <CarouselNext />
+    </Carousel>
+  );
+};
 
 export default MainCarousel;

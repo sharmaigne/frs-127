edbe7e4da4b7e82d7172b2ede1b10d8717056394
PhillipHufana <pjhufs@gmail.com>
2024-05-28 18:12:06 +0800

    feat: integrate tabs for manage requests (facility manager)

--- app/(admin)/managerequest/components/requestTabs.tsx
+++ app/(admin)/managerequest/components/requestTabs.tsx
@@ -24,45 +24,48 @@ const RequestTabs = () => {
     console.error(error);
     return <div>Error loading requests</div>;
   }
-
+  
   if (status === "pending") {
     return <div>Loading...</div>;
   }
-
-  console.log(requests)
-
+  
+  const pendingRequests = requests.filter((request) => request.endorsement_ticket_id === null && request.status !== "Withdrawn");
+  const ongoingRequests = requests.filter((request) => request.endorsement_ticket_id !== null && request.status === "Pending");
+  const finishedRequests = requests.filter((request) => request.status !== "Pending");
+  
+  
   return (
     <Tabs className="w-full" defaultValue="pending">
       <TabsList className="flex w-full justify-center border-b h-15">
         <TabsTrigger className="py-2 px-4 font-medium" value="pending">
           Pending Requests
-          <Badge className="ml-2 rounded-full bg-red-700 px-2 text-xs font-medium ">12</Badge>
+          <Badge className="ml-2 rounded-full bg-red-700 px-2 text-xs font-medium ">{pendingRequests.length}</Badge>
         </TabsTrigger>
         <TabsTrigger className="py-2 px-4 font-medium" value="ongoing">
           Ongoing Requests
-          <Badge className="ml-2 rounded-full bg-red-700 px-2 text-xs font-medium ">5</Badge>
+          <Badge className="ml-2 rounded-full bg-red-700 px-2 text-xs font-medium ">{ongoingRequests.length}</Badge>
         </TabsTrigger>
         <TabsTrigger className="py-2 px-4 font-medium" value="finished">
           Finished Requests
-          <Badge className="ml-2 rounded-full bg-red-700 px-2 text-xs font-medium ">35</Badge>
+          <Badge className="ml-2 rounded-full bg-red-700 px-2 text-xs font-medium ">{finishedRequests.length}</Badge>
         </TabsTrigger>
       </TabsList>
       <TabsContent value="pending">
         <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold mb-4">Pending Requests</h1>
-        <DataTable data={requests} />
+        <DataTable data={pendingRequests} />
     </div>
       </TabsContent>
       <TabsContent value="ongoing">
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold mb-4">Pending Requests</h1>
-        <DataTable data={requests} />
+        <DataTable data={ongoingRequests} />
     </div>
       </TabsContent>
       <TabsContent value="finished">
       <div className="container mx-auto p-4">
         <h1 className="text-2xl font-bold mb-4">Pending Requests</h1>
-        <DataTable data={requests} />
+        <DataTable data={finishedRequests} />
     </div>
       </TabsContent>
     </Tabs>

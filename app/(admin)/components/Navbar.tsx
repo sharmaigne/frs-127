b0b259c6f6073aac349c782712c818d4a1edd4f1
PhillipHufana <pjhufs@gmail.com>
2024-06-01 23:22:27 +0800

    feat: added profile options to admin

--- app/(admin)/components/Navbar.tsx
+++ app/(admin)/components/Navbar.tsx
@@ -3,6 +3,7 @@ import Icon from "@/components/Icon";
 import accountCircle from "@/public/icons/account_circle.svg";
 import NotificationsIcon from "@/public/icons/notifications";
 import TuneIcon from "@/public/icons/tune";
+import Profile from "@/app/(user)/components/Profile";
 
 const Navbar = () => {
   return (
@@ -16,13 +17,7 @@ const Navbar = () => {
       <div className="flex items-center gap-4 mr-14">
         <TuneIcon className="w-[30px] h-[30px] fill-light" />
         <NotificationsIcon className="w-[30px] h-[30px] fill-light" />
-        <Icon
-          src={accountCircle.src}
-          alt="Profile"
-          color="red"
-          width={35}
-          height={35}
-        />
+        <Profile />
       </div>
     </div>
   );

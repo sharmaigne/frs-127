8b60b821ac2a7169d38033279a57802613ff10e9
PhillipHufana <pjhufs@gmail.com>
2024-06-01 23:15:24 +0800

    changed to FacilityPopup

--- app/(user)/home/components/faciCard.tsx
+++ app/(user)/home/components/faciCard.tsx
@@ -5,7 +5,7 @@ import { Button } from "@/components/ui/button";
 import { Badge } from "@/components/ui/badge";
 import { CardContent, Card } from "@/components/ui/card";
 import dynamic from "next/dynamic";
-import FacilityPopup from "./FacilityDetailsMain";
+import FacilityPopup from "./FacilityPopup";
 
 const Popup = dynamic(() => import("./Popup"), { ssr: false });
 

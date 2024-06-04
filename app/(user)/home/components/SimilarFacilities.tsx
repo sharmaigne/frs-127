94ed1d34bdc3cef4bba2275d76b1664933a5fd5b
Sharmaigne Angelie Mabano <sharmaignemabano@gmail.com>
2024-06-05 02:34:58 +0800

    feat: align buttons

--- app/(user)/home/components/SimilarFacilities.tsx
+++ app/(user)/home/components/SimilarFacilities.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+// top 3 similar facilities based on capacity
+
 import * as React from "react";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
@@ -33,25 +35,26 @@ const SimilarFacilities: React.FC<SimilarFacilitiesProps> = ({
                   className="w-full h-48 object-cover"
                 />
               </Link>
-              <div className="p-4">
-                <h3 className="text-lg font-semibold">
-                  <Link
-                    href="#"
-                    className="hover:text-primary"
-                    prefetch={false}
-                  >
-                    {facility.name}
-                  </Link>
-                </h3>
-                <p className="text-gray-500 dark:text-gray-400">
-                  {facility.location}
-                </p>
-                <p className="text-gray-500 dark:text-gray-400">
-                  Capacity: {facility.capacity}
-                </p>
+              <div className="p-4 flex flex-col h-[200px] justify-between">
+                <div>
+                  <h3 className="text-lg font-semibold">
+                    <Link
+                      href="#"
+                      className="hover:text-primary"
+                      prefetch={false}
+                    >
+                      {facility.name}
+                    </Link>
+                  </h3>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    {facility.location}
+                  </p>
+                  <p className="text-gray-500 dark:text-gray-400">
+                    Capacity: {facility.capacity}
+                  </p>
+                </div>
                 <div className="flex justify-end mt-4">
-                  {/* add formrequest link here */}
-                  <Button>Reserve Now</Button>
+                  <Button asChild><Link href={`home/formrequest/${facility.facility_id}`}>Reserve Now</Link></Button>
                 </div>
               </div>
             </div>

1a760da4ec883dd1e14f08fe1d3f6d6d7d24c1c1
EmTampz <mmtampogao@gmail.com>
2024-06-05 16:10:34 +0800

    calendar

--- app/(admin)/managefacility/createFacility.tsx
+++ app/(admin)/managefacility/createFacility.tsx
@@ -1,63 +1,187 @@
-import { ColumnDef } from "@tanstack/react-table";
+"use client";
+// TODO: handle image upload
+
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
-import { DropdownMenu, DropdownMenuTrigger, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuSeparator } from "@/components/ui/dropdown-menu";
-import { ArrowUpDown, MoreHorizontal } from "lucide-react";
-import EditFacility from "./editFacility";
+import {
+  DialogTrigger,
+  DialogTitle,
+  DialogDescription,
+  DialogHeader,
+  DialogFooter,
+  DialogContent,
+  Dialog,
+} from "@/components/ui/dialog";
+import { Input } from "@/components/ui/input";
+import {
+  SelectValue,
+  SelectTrigger,
+  SelectItem,
+  SelectContent,
+  Select,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+
+import { z } from "zod";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+import { createFacilitySchema } from "@/lib/validators";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import useAddFacility from "@/hooks/mutations/useAddFacility";
 import { Facility } from "@/lib/types";
 
-export const columns: ColumnDef<Facility["Row"]>[] = [
-  {
-    id: "select",
-    header: ({ table }) => (
-      <Checkbox
-        checked={table.getIsAllPageRowsSelected() || (table.getIsSomePageRowsSelected() && "indeterminate")}
-        onCheckedChange={(value: boolean) => table.toggleAllPageRowsSelected(!!value)}
-        aria-label="Select all"
-        className="invisible"
-      />
-    ),
-    cell: ({ row }) => (
-      <Checkbox
-        checked={row.getIsSelected()}
-        onCheckedChange={(value: boolean) => row.toggleSelected(!!value)}
-        aria-label="Select row"
-        className="invisible"
-      />
-    ),
-    enableSorting: false,
-    enableHiding: false,
-  },
-  {
-    accessorKey: "name",
-    header: "facility",
-    cell: ({ row }) => <div className="font-bold"> {row.getValue("name")}</div>,
-  },
+type FormData = z.input<typeof createFacilitySchema>;
 
-  {
-    accessorKey: "picture",
-    header: "Image",
-    cell: ({ row }) => <div>{row.getValue("picture") }
-    </div>,
-  },
-  {
-    accessorKey: "description",
-    header: "Description",
-    cell: ({ row }) => <div>{row.getValue("description")}</div>,
-  },
-  {
-    accessorKey: "type",
-    header: "Type",
-    cell: ({ row }) => <div>{row.getValue("type")} </div>,
-  },
-  {
-    accessorKey: "location",
-    header: "Location",
-    cell: ({ row }) => <div>{row.getValue("location")}</div>,
-  },
-  {
-    accessorKey: "capacity",
-    header: "Capacity",
-    cell: ({ row }) => <div className="capacity">{row.getValue("capacity")}</div>,
-  }
-];
+const CreateFacility = () => {
+  const form = useForm<FormData>({
+    resolver: zodResolver(createFacilitySchema),
+    defaultValues: {
+      name: "",
+      facility_type: undefined,
+      description: "",
+      location: "",
+      capacity: 0,
+      image_url: "",
+    },
+  });
+
+  const { mutate } = useAddFacility();
+
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button variant="outline" className="bg-secondary-600  hover:bg-secondary-500 text-white">Add Facility</Button>
+      </DialogTrigger>
+      <DialogContent className="sm:max-w-[600px]">
+        <DialogHeader>
+          <DialogTitle>Add New Facility</DialogTitle>
+          <DialogDescription>
+            Fill out the form to add a new facility to the system.
+          </DialogDescription>
+        </DialogHeader>
+
+        <Form {...form}>
+          <form className="grid gap-4 py-4" onSubmit={form.handleSubmit((data) => {mutate(data)})}>
+            <div className="grid grid-cols-2 gap-4">
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem className="space-y-2">
+                    <FormLabel>Facility Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter facility name" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="facility_type"
+                render={({ field }) => (
+                  <FormItem className="space-y-2">
+                    <FormLabel>Facility Type</FormLabel>
+                    <FormControl>
+                      <Select {...field}>
+                        <SelectTrigger>
+                          <SelectValue placeholder="Select facility type" />
+                        </SelectTrigger>
+                        <SelectContent>
+                          <SelectItem value="hall">Hall</SelectItem>
+                          <SelectItem value="classroom">Classroom</SelectItem>
+                          <SelectItem value="court">Court</SelectItem>
+                          <SelectItem value="field">
+                            Open Fields
+                          </SelectItem>
+                        </SelectContent>
+                      </Select>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <FormField
+              control={form.control}
+              name="description"
+              render={({ field }) => (
+                <FormItem className="space-y-2">
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Textarea
+                      placeholder="Enter facility description"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+
+            <div className="grid grid-cols-2 gap-4">
+              <FormField
+                control={form.control}
+                name="location"
+                render={({ field }) => (
+                  <FormItem className="space-y-2">
+                    <FormLabel>Location</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter facility location" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormField
+                control={form.control}
+                name="capacity"
+                render={({ field }) => (
+                  <FormItem className="space-y-2">
+                    <FormLabel>Capacity</FormLabel>
+                    <FormControl>
+                      <Input
+                        type="number"
+                        placeholder="Enter facility capacity"
+                        {...field}
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </div>
+
+            <FormField
+              control={form.control}
+              name="image_url"
+              render={({ field }) => (
+                <FormItem className="space-y-2">
+                  <FormLabel>Upload Picture</FormLabel>
+                  <FormControl>
+                    <Input type="file" {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <DialogFooter>
+              <Button type="submit">Save Facility</Button>
+            </DialogFooter>
+          </form>
+        </Form>
+      </DialogContent>
+    </Dialog>
+  );
+};
+export default CreateFacility;

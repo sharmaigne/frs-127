0179896ea657f4bdab7283a3da2c9119c0b816dd
Sharmaigne Angelie Mabano <sharmaignemabano@gmail.com>
2024-06-05 15:01:57 +0800

    fix: setOpen logic error

--- app/(user)/requests/DraftRequest.tsx
+++ app/(user)/requests/DraftRequest.tsx
@@ -29,7 +29,7 @@ const DraftRequest = ({ request }: { request: Request["Row"] }) => {
   const defaultCircle = (
     <span className="rounded-full w-3 h-3 border border-darker bg-text-200" />
   );
-  
+
   const [open, setOpen] = useState(false);
 
   // TODO: fix bug with handling open state
@@ -40,33 +40,34 @@ const DraftRequest = ({ request }: { request: Request["Row"] }) => {
   };
 
   return (
-    <div
-      className="rounded-lg p-4 hover:bg-dark/10 min-w-[50%] cursor-pointer"
-      onClick={handleViewDetails}
-    >
+    <div>
       <ViewDetailsDialog
         open={open}
         handleOpen={handleViewDetails}
         request={request}
       />
-
-      <div className="border-l-2 border-darker px-4 py-2">
-        <h5 className="font-bold">{getFacilityName()}</h5>
-        <DateLine
-          dateStart={request.timestamp_start}
-          dateEnd={request.timestamp_end}
-        />
-        <div className="rounded bg-secondary-500/20 p-3 mt-2">
-          <div className="flex items-center gap-2">
-            <p className="font-bold">Activity Request</p>
+      <div
+        className="rounded-lg p-4 hover:bg-dark/10 min-w-[50%] cursor-pointer"
+        onClick={handleViewDetails}
+      >
+        <div className="border-l-2 border-darker px-4 py-2">
+          <h5 className="font-bold">{getFacilityName()}</h5>
+          <DateLine
+            dateStart={request.timestamp_start}
+            dateEnd={request.timestamp_end}
+          />
+          <div className="rounded bg-secondary-500/20 p-3 mt-2">
+            <div className="flex items-center gap-2">
+              <p className="font-bold">Activity Request</p>
+            </div>
+            <div className="flex items-center gap-2 mt-2">
+              <p className="font-bold">Venue Request</p>
+            </div>
           </div>
-          <div className="flex items-center gap-2 mt-2">
-            <p className="font-bold">Venue Request</p>
-          </div>
-        </div>
-        <div className="rounded bg-accent-100 p-3 mt-2">
-          <div className="flex items-center gap-2">
-            <p className="font-bold">Risk Analysis</p>
+          <div className="rounded bg-accent-100 p-3 mt-2">
+            <div className="flex items-center gap-2">
+              <p className="font-bold">Risk Analysis</p>
+            </div>
           </div>
         </div>
       </div>
--- app/(user)/requests/OngoingRequest.tsx
+++ app/(user)/requests/OngoingRequest.tsx
@@ -21,59 +21,60 @@ const OngoingRequest = ({ request }: { request: Request["Row"] }) => {
   };
 
   return (
-    <div className="hover:bg-dark/10 p-4 rounded-lg cursor-pointer">
+    <div>
       <ViewDetailsDialog
         open={open}
         handleOpen={handleViewDetails}
         request={request}
       />
-      <div
-        key={request.request_id}
-        className="border-l-2 pl-4 pb-4 border-l-darker flex-col items-center"
-      >
-        <h5 className="font-bold">{getFacilityName()}</h5>
-        <p className="text-primary ">{request.event_name}</p>
-        <DateLine
-          dateStart={request.timestamp_start}
-          dateEnd={request.timestamp_end}
-          // className="text-lg"
-        />
-
-        {/* drawing thing */}
-        <div className="grid grid-cols-5 mt-6 text-sm">
-          <div className="col-span-1 flex-col">
-            <ProgressIcon status="approved" />
-            <p className="font-bold mt-2 col-span-1 text-center">
-              Request Sent
-            </p>
-          </div>
-          <div className="col-span-1 flex-col">
-            <ProgressIcon status="pending" />
-            <p className="font-bold mt-2 col-span-1 text-center">
-              Approved by <br />
-              Venue Coordinator
-            </p>
-          </div>
-          <div className="col-span-1 flex-col">
-            <ProgressIcon status="pending" />
-            <p className="font-bold mt-2 col-span-1 text-center">
-              Forms sent to <br />
-              SOAS
-            </p>
-          </div>
-          <div className="col-span-1 flex-col">
-            <ProgressIcon status="rejected" />
-            <p className="font-bold mt-2 col-span-1 text-center">
-              Approved by <br />
-              SOAS
-            </p>
-          </div>
-          <div className="col-span-1 flex-col">
-            <ProgressIcon status="" />
-            <p className="font-bold mt-2 col-span-1 text-center">
-              Approved by <br />
-              OSA
-            </p>
+      <div className="hover:bg-dark/10 p-4 rounded-lg cursor-pointer" onClick={handleViewDetails}>
+        <div
+          key={request.request_id}
+          className="border-l-2 pl-4 pb-4 border-l-darker flex-col items-center"
+        >
+          <h5 className="font-bold">{getFacilityName()}</h5>
+          <p className="text-primary ">{request.event_name}</p>
+          <DateLine
+            dateStart={request.timestamp_start}
+            dateEnd={request.timestamp_end}
+            // className="text-lg"
+          />
+          {/* drawing thing */}
+          <div className="grid grid-cols-5 mt-6 text-sm">
+            <div className="col-span-1 flex-col">
+              <ProgressIcon status="approved" />
+              <p className="font-bold mt-2 col-span-1 text-center">
+                Request Sent
+              </p>
+            </div>
+            <div className="col-span-1 flex-col">
+              <ProgressIcon status="pending" />
+              <p className="font-bold mt-2 col-span-1 text-center">
+                Approved by <br />
+                Venue Coordinator
+              </p>
+            </div>
+            <div className="col-span-1 flex-col">
+              <ProgressIcon status="pending" />
+              <p className="font-bold mt-2 col-span-1 text-center">
+                Forms sent to <br />
+                SOAS
+              </p>
+            </div>
+            <div className="col-span-1 flex-col">
+              <ProgressIcon status="rejected" />
+              <p className="font-bold mt-2 col-span-1 text-center">
+                Approved by <br />
+                SOAS
+              </p>
+            </div>
+            <div className="col-span-1 flex-col">
+              <ProgressIcon status="" />
+              <p className="font-bold mt-2 col-span-1 text-center">
+                Approved by <br />
+                OSA
+              </p>
+            </div>
           </div>
         </div>
       </div>
